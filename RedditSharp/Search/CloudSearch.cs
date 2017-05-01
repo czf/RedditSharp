f2d9a7475d9dd759055dddd975e76886d7b71954
Chris F <fingarc@gmail.com>
2017-04-30 20:27:45 -0700

    Working though unit tests
    adding new cloudsearchfilter and filter method to take a func and create
    a search querystring.

---
+++ RedditSharp/Search/CloudSearch.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using RedditSharp.Things;
+namespace RedditSharp.Search
+{
+    /// <summary>
+    /// https://github.com/reddit/reddit/blob/master/r2/r2/lib/providers/search/common.py
+    /// </summary>
+    public interface ICloudSearchFilter
+    {
+        CompoundSearchOperator GetCompoundSearchOperator();
+
+        ICloudSearchFilter AddAndFilter(ICloudSearchFilter filter);
+        ICloudSearchFilter AddOrFilter(ICloudSearchFilter filter);
+
+        ICloudSearchFilter AddUtcDateRange(DateTime from, DateTime to);
+        ICloudSearchFilter AddUtcDateRanges(IEnumerable<CloudSearchFilter.UtcDateRange> utcDateRanges);
+
+        ICloudSearchFilter Over18(bool over18);
+        //ICloudSearchFilter
+    }
+
+    public class CloudSearchFilter
+    {
+        public string title;
+        public string subreddit;
+        public string flair_text;
+        
+        public int ups;
+        public bool over18;
+
+        public bool timestamp(DateTime? from, DateTime? end)
+        {
+            return true;
+        }
+
+        public class UtcDateRange
+        {
+            private UtcDateRange() { }
+            //public DateTime From { get; set; }
+            //public DateTime To { get; set; }
+            public bool Between(DateTime? from, DateTime? end)
+            {
+                return true;
+            }
+            //public static bool operator ==(DateTime filter, UtcDateRange dateRange)
+            //{
+            //    return true;
+            //}
+            //public static bool operator !=(DateTime filter, UtcDateRange dateRange)
+            //{
+            //    return true;
+            //}
+        }
+        public static string Filter(Expression<Func<CloudSearchFilter, bool>> expression)
+        {
+            var x = FilterCallbinder(expression.Body);
+
+            return x;
+        }
+        public static string Filter(Expression<Func<CloudSearchFilter, bool>> expression, Expression<Func<CloudSearchFilter, bool>> expression2, Expression<Func<CloudSearchFilter, bool>> expression3, Expression<Func<CloudSearchFilter, bool>> expression4)
+        {
+            var x = FilterCallbinder(expression4.Body);
+
+            return x;
+        }
+        
+        
+        public static string FilterHelper(UnaryExpression expression)
+        {
+            if(expression.NodeType != ExpressionType.Not)
+            { throw new NotSupportedException("Only supports UnaryExpression for 'Not'"); }
+
+            string result = string.Empty;
+
+            if(expression.Operand.NodeType == ExpressionType.MemberAccess)
+            {
+                MemberExpression member = expression.Operand as MemberExpression;
+                result = GetMemberFilter(member, false);
+            }
+            else
+            {
+                string op = expression.NodeType.ToOperator();
+
+                result = string.Format(op, FilterCallbinder(expression.Operand));
+            }
+            return result;
+
+
+        }
+
+        private static readonly List<ExpressionType> conditionalTypes = new List<ExpressionType>()
+        {
+            ExpressionType.AndAlso,
+            ExpressionType.And,
+            ExpressionType.OrElse,
+            ExpressionType.Or
+        };
+
+        public static string FilterHelper(BinaryExpression expression)
+        {
+            string op = expression.NodeType.ToOperator();
+            string l = null;
+            string r = null;
+            if (conditionalTypes.Any(x => x == expression.NodeType))
+            {
+                
+                if (expression.NodeType == expression.Left.NodeType)
+                {
+                    BinaryExpression left =  expression.Left as BinaryExpression;
+                    l = FilterCallbinder(left.Left) + "+" + FilterCallbinder(left.Right);
+                }
+
+                if (expression.NodeType == expression.Right.NodeType)
+                {
+                    BinaryExpression right = expression.Left as BinaryExpression;
+                    r = FilterCallbinder(right.Left) + "+" + FilterCallbinder(right.Right);
+                }
+                
+
+            }
+         
+
+            return string.Format(op, l ?? FilterCallbinder(expression.Left),
+                r ?? FilterCallbinder(expression.Right));
+            
+        }
+
+        private static string FilterHelper(MethodCallExpression call)
+        {
+            string from = null;
+            string end = null;
+            string result = string.Empty;
+            if(call.Method.Name == nameof(CloudSearchFilter.timestamp))
+            {
+                from = FilterCallbinder(call.Arguments[0]);
+                end = FilterCallbinder(call.Arguments[1]);
+                result = $"timestamp:{from}..{end}";
+            }
+
+            return result;
+
+        }
+        
+        public static string FilterHelper(MemberExpression member)
+        {
+            string result = string.Empty;
+            result = GetMemberFilter(member,true);
+
+            return result;
+        }
+
+        private static string GetMemberFilter(MemberExpression member, bool filterValue)
+        {
+            string result;
+            
+            if (member.Type == typeof(bool))
+            {
+                result = $"{member.Member.Name}:{(filterValue ? "1": "0")}";
+            }
+             else if (member.Member.DeclaringType != typeof(CloudSearchFilter))
+            {
+                result = member.InvokeGet().ToString();
+            }
+            else if(member.Expression is ConstantExpression)
+            {
+                result = FilterHelper(member.Expression as ConstantExpression);
+            }
+           
+            else
+            {
+                result = member.Member.Name;
+            }
+
+            return result;
+        }
+
+        public static string FilterHelper(ConstantExpression constant)
+        {
+
+            return constant.Value.ToString();
+        }
+
+        // using dynamic to hack late binding so i don't have to 
+        // write a bunch of "if is type then cast and call cases".
+        private static string FilterCallbinder(dynamic expression)
+        {
+            return FilterHelper(expression);
+        }
+
+    }
+
+    public enum CompoundSearchOperator
+    {
+        None,
+        And,
+        Or
+
+    }
+    public static class Extensions
+    {
+        public static string ToOperator(this ExpressionType type)
+        {
+            string result = string.Empty;
+            switch (type)
+            {
+                case ExpressionType.And:
+                    break;
+                case ExpressionType.AndAlso:
+                    result = "(and+{0}+{1})";
+                    break;
+                case ExpressionType.Conditional:
+                    break;
+                case ExpressionType.Equal:
+                    result = "{0}:{1}";
+                    break;
+                case ExpressionType.ExclusiveOr:
+                    break;
+                case ExpressionType.GreaterThan:
+                    result = "{0}:{1}.."; // extra processing needed to adjust
+                    break;
+                case ExpressionType.GreaterThanOrEqual:
+                    result = "{0}:{1}..";
+                    break;
+                case ExpressionType.LessThan:
+                    result = "{0}:..{1}"; // extra processing needed to adjust
+                    break;
+                case ExpressionType.LessThanOrEqual:
+                    result = "{0}:..{1}";  
+                    break;
+                case ExpressionType.MemberAccess:
+                    break;
+                case ExpressionType.Negate:
+                    break;
+                case ExpressionType.NegateChecked:
+                    break;
+                case ExpressionType.Not:
+                    result = "(not+{0})";
+                    break;
+                case ExpressionType.NotEqual:
+                    break;
+                case ExpressionType.Or:
+                    break;
+                case ExpressionType.OrElse:
+                    result = "(or+{0}+{1})";
+                    break;
+                case ExpressionType.IsTrue:
+                    break;
+                case ExpressionType.IsFalse:
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="member"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// source : http://stackoverflow.com/a/2616980
+        /// </remarks>
+        public static object InvokeGet(this MemberExpression member)
+        {
+            var objectMember = Expression.Convert(member, typeof(object));
+
+            var getterLambda = Expression.Lambda<Func<object>>(objectMember);
+
+            var getter = getterLambda.Compile();
+
+            return getter();
+        }
+    }
+}
---
+++ UnitTesting/CloudSearchTest.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RedditSharp.Things;
+using RedditSharp;
+using System.Net;
+using UnitTesting.TestData;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Linq.Expressions;
+using RedditSharp.Search;
+namespace UnitTesting
+{
+    /// <summary>
+    /// Summary description for CloudSearchTest
+    /// </summary>
+    [TestClass]
+    public class CloudSearchTest
+    {
+        public CloudSearchTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void BoolPropertyTest()
+        {
+            //Arrange
+            Expression<Func<CloudSearchFilter, bool>> 
+                expression = x => !x.over18;
+            string expected = "over18:0";
+            
+
+            //Act
+            string actual = CloudSearchFilter.Filter(expression);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PropertyGreaterThanVariable()
+        {
+            //Arrange
+            int minUps = IntVal();
+
+            Expression<Func<CloudSearchFilter, bool>>
+                expression = x => !(x.ups > minUps);
+            string expected = $"(not+ups:{minUps+1}..)";
+
+            //Act
+            string actual = CloudSearchFilter.Filter(expression);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PropertyGreaterThanOrEqualVariable()
+        {
+            //Arrange
+            int minUps = IntVal();
+
+            Expression<Func<CloudSearchFilter, bool>>
+                expression = x => !(x.ups >= minUps);
+            string expected = $"(not+ups:{minUps}..)";
+
+            //Act
+            string actual = CloudSearchFilter.Filter(expression);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TimestampEndOnlyTest()
+        {
+            //Arrange
+            DateTime end = new DateTime(2017, 3, 1);
+            DateTimeOffset endOffset = new DateTimeOffset(end);
+            Expression<Func<CloudSearchFilter, bool>>
+                expression = x => x.timestamp(null, end);
+            string expected = $"timestamp:..{endOffset.ToUnixTimeSeconds()}";
+
+
+            //Act
+            string actual = CloudSearchFilter.Filter(expression);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void TimestampStartOnlyTest()
+        {
+            //Arrange
+            DateTime start = new DateTime(2017, 1, 1);
+            DateTimeOffset startOffset = new DateTimeOffset(start);
+            Expression<Func<CloudSearchFilter, bool>>
+                expression = x => x.timestamp(start, null);
+            string expected = $"timestamp:{startOffset.ToUnixTimeSeconds()}..";
+
+
+            //Act
+            string actual = CloudSearchFilter.Filter(expression);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TimestampRangeTest()
+        {
+
+
+            //Arrange
+            DateTime start = new DateTime(2017, 1, 1);
+            DateTime end = new DateTime(2017, 3, 1);
+            DateTimeOffset endOffset = new DateTimeOffset(end);
+            DateTimeOffset startOffset = new DateTimeOffset(start);
+            Expression<Func<CloudSearchFilter, bool>>
+                expression = x => x.timestamp(start, end);
+            string expected = $"timestamp:{startOffset.ToUnixTimeSeconds()}..{endOffset.ToUnixTimeSeconds()}";
+
+
+            //Act
+            string actual = CloudSearchFilter.Filter(expression);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OrTest()
+        {
+            //Arrange
+            string name = CloudSearchTest.NameVal();
+            Expression<Func<CloudSearchFilter, bool>>
+                expression = x => x.subreddit == name || x.title == "'title'" || x.flair_text == "'flair text'";
+            string expected = $"(or+subreddit:{name}+title:'title'+flair_text:'flair text')";
+
+
+            //Act
+            string actual = CloudSearchFilter.Filter(expression);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Or_AndTest()
+        {
+            //Arrange
+            string name = CloudSearchTest.NameVal();
+            Expression<Func<CloudSearchFilter, bool>>
+                expression = x => x.subreddit == name || x.title == "'title'" && x.flair_text == "'flair text'";
+            string expected = $"(or+subreddit:{name}+(and+title:'title'+flair_text:'flair text'))";
+
+
+            //Act
+            string actual = CloudSearchFilter.Filter(expression);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CompoundConditionTest()
+        {
+            //Arrange
+            string name = CloudSearchTest.NameVal();
+            DateTime start = new DateTime(2017, 1, 1);
+            DateTime end = new DateTime(2017, 3, 1);
+            DateTimeOffset endOffset = new DateTimeOffset(end);
+            DateTimeOffset startOffset = new DateTimeOffset(start);
+            Expression<Func<CloudSearchFilter, bool>>
+                expression = x => (x.timestamp(start, end) || x.subreddit == name || (x.title == "title" && ((x.flair_text == "govt" && x.ups > 4) || (x.ups > 5 && x.flair_text == "news"))));
+            string expected = $"(or+timestamp:{startOffset.ToUnixTimeSeconds()}..{endOffset.ToUnixTimeSeconds()}+subreddit:{name}+(and+title:title+(or+(and+flair_text:govt+ups:5..)+(and+ups:5..+flair_text:news))))";
+            //%28or+timestamp:1483257600..1488355200+subreddit:%27SeattleWA%27+%28and+title:%27title%27+%28or+%28and+flair_text:%27govt%27+ups:5..%29+%28and+ups:5..+flair_text:%27news%27%29%29%29%29
+            //Act
+            string actual = CloudSearchFilter.Filter(expression);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //    DateTime dt = new DateTime(2017, 1, 1);
+        //    DateTime dt2 = new DateTime(2017, 3, 1);
+        //    string f = CloudSearchTest.NameVal();
+        //    int u = IntVal();
+        //    CloudSearchFilter.Filter(
+        //        x => !(x.ups > u),
+        //        //x=>true, 
+        //        x => !x.over18, //try negation too
+        //        x => x.subreddit == f || x.Title == "'title'" && x.flair_text == "'flair text'",
+        //        //x => (x.Timestamp > dt && x.Timestamp < dt2) || x.subreddit == f || (x.Title == "title" && ((x.flair_text == "govt" && x.ups > 4) || (x.ups > 5 && x.flair_text == "news")))
+        //        x => x.TimestampFilter(null, DateTime.Now)
+        //        );
+        //}
+        private static string NameVal()
+        {
+            return "'SeattleWA'";
+        }
+        private static int IntVal()
+        {
+            return 4;
+        }
+    }
+}
